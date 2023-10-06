b141deb1d9f0a39ccd5e9b1e98fa48147c48f176
zjhamm304@gmail.com <zach.hamm@validic.com>
2023-10-06 13:41:33 -0400

    init

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+const {defineConfig} = require("cypress");
+
+module.exports = defineConfig({
+    e2e: {
+        async setupNodeEvents(on, config) {
+            return config;
+        },
+        specPattern: `cypress/tests/**/*.spec.js`,
+        baseUrl: `https://www.cypress.io/`,
+        defaultCommandTimeout: 30000,
+        numTestsKeptInMemory: 1,
+        chromeWebSecurity: false,
+        testIsolation: true,
+    }
+});
